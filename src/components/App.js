936cd5f74c39ba5371180b2b109498ce5f6e8b0f
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2025-01-20 14:32:43 +0530

    App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,10 @@ import React, { useState } from "react";
 const ButtonCounter = () => {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <p>Button clicked {count} times.</p>
-      <button onClick={handleClick} style={{ padding: "10px", fontSize: "16px" }}>
-        Click Me
-      </button>
+    <div>
+      <p>Button clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
     </div>
   );
 };
